6d3c410a14cbf76e3c6a00c23648fc4f4e90daf2
Joana Marques <joana.pipaa@gmail.com>
2019-01-03 12:52:26 +0000

    Add Enemy car class

---
+++ LP2_P2/EnemyCar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LP2_P2
+{
+    public class EnemyCar : IGameObject
+    {
+        RandomGenerator rand = new RandomGenerator();
+
+        public int x;
+        public int y;
+        public char c;
+        public ConsoleColor color;
+
+        public EnemyCar()
+        {
+            x = rand.RandomObjectXPosition();
+            y = 0;
+            c = '#';
+            color = ConsoleColor.Green;
+        }
+
+        public void Update()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(c);
+        }
+    }        
+}
