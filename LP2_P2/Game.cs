f768ac7d196a440c50d3e819134adc08827aa6ef
Pedro Santos <pedrosantosobral@gmail.com>
2019-01-03 14:24:56 +0000

    Add Game class and spawn game objects randomnly

---
+++ LP2_P2/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LP2_P2
+{
+    public class Game
+    {
+        public int livesCount = 5;
+        public double speed = 100.0;
+        public double acceleration = 0.5;
+
+        public void GameManager()
+        {
+            bool startgame = true;
+            Random rand = new Random();
+            UserCar userCar = new UserCar();
+
+            List<IGameObject> objects = new List<IGameObject>();
+            while (startgame == true)
+            {
+                userCar.Update();
+                speed += acceleration;
+                if (speed > 400)
+                {
+                    speed = 400;
+                }
+
+                bool hitted = false;
+                {
+                    int chance = rand.Next(0, 100);
+
+                    if (chance < 10)
+                    {
+                        Life life = new Life
+                        {
+                            x = rand.Next(0, 5)
+                        };
+                        objects.Add(life);
+                    }
+                    else
+                    {
+                        EnemyCar enemyCar = new EnemyCar
+                        {
+                            x = rand.Next(0, 5)
+                        };
+                        objects.Add(enemyCar);
+                    }
+
+                }
+            
+            Thread.Sleep((int)(600 - speed));
+            }
+        }
+    }
+}
+
+
+
