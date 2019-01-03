81bf8e86776a4660cff26886592af160167bcff9
Joana Marques <joana.pipaa@gmail.com>
2019-01-03 15:26:46 +0000

    Add render class with methods for printing

---
+++ LP2_P2/Render.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LP2_P2
+{
+    public class Render
+    {
+        public Game g;
+
+        public Render(Game g)
+        {
+            this.g = g;
+        }
+
+        public void PrintOnPosition(int x, int y, char c, ConsoleColor color = ConsoleColor.Black)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(c);
+        }
+
+        public void PrintStringOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.Black)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(str);
+        }
+
+    }
+}
+
