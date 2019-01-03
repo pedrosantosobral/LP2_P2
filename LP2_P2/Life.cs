f7f14ae0d6a16157f36cf10d4245ea84e1fc8449
Pedro Santos <pedrosantosobral@gmail.com>
2019-01-03 13:00:19 +0000

    Add game object new life

---
+++ LP2_P2/Life.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace LP2_P2
+{
+    public class Life : IGameObject
+    {
+        RandomGenerator rand = new RandomGenerator();
+
+        public int x;
+        public int y;
+        public char c;
+        public ConsoleColor color;
+
+        public Life()
+        {
+            x = rand.RandomObjectXPosition();
+            y = 0;
+            c = '-';
+            color = ConsoleColor.Green;
+        }
+
+        public void Update()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(c);
+        }
+    }
+}
