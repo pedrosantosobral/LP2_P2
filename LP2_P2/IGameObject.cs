706c2619ac2f3be313de78a2c02a4f827ee5805c
Joana Marques <joana.pipaa@gmail.com>
2019-01-05 21:11:44 +0000

    Add commentaries in IGameObject and Program class

--- LP2_P2/IGameObject.cs
+++ LP2_P2/IGameObject.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 namespace LP2_P2
 {
+    /// <summary>
+    /// Game object interface.
+    /// </summary>
     public interface IGameObject
     {
+        /// <summary>
+        /// Method to be used by other classes that implement IGameObject.
+        /// </summary>
         void Update();
     }
 }
--- LP2_P2/Program.cs
+++ LP2_P2/Program.cs
@@ -6,15 +6,30 @@ using System.Threading;
 
 namespace LP2_P2
 {
+    /// <summary>
+    /// Program class (initializes the game).
+    /// </summary>
     class Program
     {
-
+        /// <summary>
+        /// The entry point of the program, where the program control starts 
+        /// and ends.
+        /// </summary>
         static void Main()
         {
+            /// Game instance.
             Game g = new Game();
+
+            /// Render instance.
             Render r = new Render(g);
+
+            /// Calls the MainMenu from Render class.
             r.MainMenu();
+
+            /// Waits for the user to click on a key to pass to continue.
             Console.ReadKey();
+
+            /// calls the GameManager from the Game class
             g.GameManager();
         }
     }
