11aa8c00946247ce982e67c3c27811db3b8f4e82
Joana Marques <joana.pipaa@gmail.com>
2019-01-05 21:49:59 +0000

    Add comentaries in Life class

--- LP2_P2/Life.cs
+++ LP2_P2/Life.cs
@@ -2,15 +2,39 @@
 using System.Collections;
 namespace LP2_P2
 {
+    /// <summary>
+    /// Life class.
+    /// </summary>
     public class Life : IGameObject
     {
+        /// <summary>
+        /// Random generator class instance
+        /// </summary>
         RandomGenerator rand = new RandomGenerator();
 
+        /// <summary>
+        /// The life position x.
+        /// </summary>
         public int posX;
+
+        /// <summary>
+        /// The life position y.
+        /// </summary>
         public int posY;
+
+        /// <summary>
+        /// The life visuals.
+        /// </summary>
         public char visuals;
+
+        /// <summary>
+        /// The life color.
+        /// </summary>
         public ConsoleColor color;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:LP2_P2.Life"/> class.
+        /// </summary>
         public Life()
         {
             posX = rand.RandomObjectXPosition();
@@ -19,6 +43,9 @@ namespace LP2_P2
             color = ConsoleColor.Green;
         }
 
+        /// <summary>
+        /// Object position update method.
+        /// </summary>
         public void Update()
         {
             Console.SetCursorPosition(posX, posY);
